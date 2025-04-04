6e56fe138ff757cd069f187da62457ed2f0ccbcb
biel10yt <gabrielsoares25022005@gmail.com>
2025-04-04 19:28:32 -0300

    up

--- app/validacao.py
+++ app/validacao.py
@@ -7,16 +7,15 @@ def validar_cpf(cpf: str) -> bool:
     :param cpf: CPF como string (somente números ou no formato xxx.xxx.xxx-xx)
     :return: True se for válido, False caso contrário
     """
-    cpf = re.sub(r'\D', '', cpf)  # Remove caracteres não numéricos
+    cpf = re.sub(r'\D', '', cpf)  
 
     if len(cpf) != 11 or cpf == cpf[0] * 11:
-        return False  # CPF inválido se todos os números forem iguais
-
-    # Cálculo do primeiro dígito verificador
+        return False  
+   
     soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
     digito1 = (soma * 10 % 11) % 10
 
-    # Cálculo do segundo dígito verificador
+    
     soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
     digito2 = (soma * 10 % 11) % 10
 
--- tests/test_validacao.py
+++ tests/test_validacao.py
@@ -3,19 +3,18 @@ from app.validacao import validar_cpf
 
 class TestValidarCPF(unittest.TestCase):
     def test_cpf_valido(self):
-        self.assertTrue(validar_cpf("12345678909"))  # Exemplo fictício de CPF válido
-
+        self.assertTrue(validar_cpf("12345678909")) 
     def test_cpf_invalido(self):
-        self.assertFalse(validar_cpf("12345678900"))  # CPF inválido
+        self.assertFalse(validar_cpf("12345678900"))
 
     def test_cpf_com_caracteres_extras(self):
-        self.assertTrue(validar_cpf("123.456.789-09"))  # CPF válido com pontuação
+        self.assertTrue(validar_cpf("123.456.789-09"))
 
     def test_cpf_todos_iguais(self):
-        self.assertFalse(validar_cpf("11111111111"))  # CPFs com todos os dígitos iguais são inválidos
+        self.assertFalse(validar_cpf("11111111111"))
 
     def test_cpf_curto(self):
-        self.assertFalse(validar_cpf("12345"))  # CPF com menos de 11 dígitos é inválido
-
+        self.assertFalse(validar_cpf("12345"))
+        
 if __name__ == "__main__":
     unittest.main()
